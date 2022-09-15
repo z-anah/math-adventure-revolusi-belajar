9cec375f8cfb0bbac3893e232082dfb2a447b877
anah <zulmianah@gmail.com>
2022-09-15 07:57:35 +0300

    init

---
+++ Assets/Scripts/ScneManagement/SceneController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(string newSceneName){
+        SceneManager.LoadScene(newSceneName);
+    }
+}
