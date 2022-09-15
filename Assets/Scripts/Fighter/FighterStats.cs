f08c2a4a12cfbbaa42c010a173f908c87aae7c73
anah <zulmianah@gmail.com>
2022-09-15 15:07:36 +0300

    attack

--- Assets/Scripts/Fighter/FighterStats.cs
+++ Assets/Scripts/Fighter/FighterStats.cs
@@ -1,5 +1,8 @@
 using System;
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+using System.Collections;
 
 public class FighterStats : MonoBehaviour
 {
@@ -8,21 +11,70 @@ public class FighterStats : MonoBehaviour
     public double Hp;
     public Stat AttackDamage;
     public Stat AttackInterval;
+
+    private TextMeshPro statText;
+
     private void Awake()
     {
+        statText = transform.Find("StatText").GetComponent<TextMeshPro>();
         Hp = MaxHp;
-    }
-    private void TakeDamage(int attackDamage)
-    {
-        Hp -= attackDamage;
-
-        if (Hp <= 0)
-        {
-            Die();
-        }
+        StartCoroutine(Attack());
     }
     private void Die()
     {
-        Debug.Log("DIE :"+Label);
+        Debug.Log("DIE :" + Label);
+    }
+    public void Update()
+    {
+        statText.text = $"{Label}\nAttack damage: {AttackDamage.Value}\nAttack Interval: {AttackInterval.Value} sec\nHP: {Hp}/{MaxHp}";
+        if (Hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        if (isActiveAndEnabled)
+        {
+            GameObject oToAttack = getClosetsFighter();
+            if (oToAttack == null)
+            {
+                Debug.Log("DONE !");
+            }
+            else
+            {
+                FighterStats toAttack = oToAttack.GetComponent<FighterStats>();
+                Debug.Log($"{Label} ATTACK {toAttack.Label}");
+                toAttack.Hp -= AttackDamage.Value;
+            }
+            yield return new WaitForSeconds((float)AttackInterval.Value);
+            StartCoroutine(Attack());
+        }
+    }
+
+    private GameObject getClosetsFighter()
+    {
+        float oldDistance = Mathf.Infinity;
+        GameObject team = null;
+        team = GameObject.Find(transform.tag == "Hero" ? "Enemies" : "Heroes");
+        List<GameObject> fightersTeam = new List<GameObject>();
+        int children = team.transform.childCount;
+        for (int i = 0; i < children; ++i)
+        {
+            GameObject item = team.transform.GetChild(i).gameObject;
+            if (item.activeSelf) fightersTeam.Add(item);
+        }
+        GameObject closetsFighter = null;
+        foreach (GameObject item in fightersTeam)
+        {
+            float dist = Vector3.Distance(this.gameObject.transform.position, item.transform.position);
+            if (dist < oldDistance)
+            {
+                closetsFighter = item;
+                oldDistance = dist;
+            }
+        }
+        return closetsFighter;
     }
 }
